161c214dbab4256802fd5169c3b334b2a470b2b1
ss-aa123 <neermas1@hotmail.com>
2023-01-17 02:10:04 -0500

    modified button to conditionally render styles based on state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,14 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
    
-   let buttonClass = "button";
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
- 
-   
    return (
       <button 
          className={buttonClass} 
