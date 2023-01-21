07b992835cfd042a352d5f3605b754dd964fa35a
ss-aa123 <neermas1@hotmail.com>
2023-01-20 22:55:23 -0500

    update prop names

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,22 +3,21 @@ import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
   
-  const {days} = props
-  const daysData = days.map(day => {
+  const days = props.days.map(day => {
     return (
       <DayListItem 
         key={day.id} 
         name={day.name} 
         spots={day.spots} 
-        selected={day.name === props.day} 
-        setDay={props.setDay}
+        selected={day.name === props.value} 
+        setDay={props.onChange}
       />
     );
   });
 
   return (
     <ul>
-      {daysData}
+      {days}
     </ul>
   );
 }
